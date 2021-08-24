297013e2d77eb26407158f8d66469e1c3e2a4359
Joan Amudu <joan.baare@gmail.com>
2021-08-24 11:32:32 +0000

    Linking html pages

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -18,10 +19,40 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
 @app.route("/get_posts")
 def get_posts():
     posts = mongo.db.blog.find()
     return render_template("blog.html", posts=posts)
+
+
+@app.route("/my_page")
+def my_page():
+    return render_template("my_page.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    return render_template("logout.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact_us.html")
 
 
 if __name__ == "__main__":
