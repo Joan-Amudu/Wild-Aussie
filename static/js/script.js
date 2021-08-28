827239385058bcb61a4c9f40775568246b0d4357
Joan Amudu <joan.baare@gmail.com>
2021-08-28 21:12:12 +0000

    Add date picker script

--- static/js/script.js
+++ static/js/script.js
@@ -8,4 +8,14 @@ $(document).ready(function(){
     $('.tooltipped').tooltip();
     $('.parallax').parallax();
     $('.dropdown-trigger').dropdown();
+    $('select').formSelect();
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+
+    });
   });
